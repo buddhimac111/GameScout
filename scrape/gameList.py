f970fd9695fb22a74692aa5d5597ea93e5920a4f
buddhimac111 <91274532+buddhimac111@users.noreply.github.com>
2024-04-22 18:39:39 +0530

    💄: UI improvements

--- scrape/gameList.py
+++ scrape/gameList.py
@@ -1,5 +1,5 @@
-# game_list = ['alan wake', 'sifu', 'saints row','inside', 'need for speed unbound', 'blazing sails', 
-#              'rimworld', 'dead by daylight','sherlock holmes chapter one', 'the settlers',
-#              'age of wonders 4']
+game_list = ['alan wake', 'sifu', 'saints row','inside', 'need for speed unbound', 'blazing sails', 
+             'rimworld', 'dead by daylight','sherlock holmes chapter one', 'the settlers',
+             'age of wonders 4']
 
-game_list = ['age of wonders 4']
+# game_list = ['age of wonders 4']
