bf95b75f4b51c30ca4917a5c5f7f7757587ffa67
buddhimac111 <91274532+buddhimac111@users.noreply.github.com>
2024-05-01 01:03:27 +0530

    ✨: view single game

--- frontend/src/app/components/CardOne.jsx
+++ frontend/src/app/components/CardOne.jsx
@@ -1,9 +1,22 @@
 import { Card } from "flowbite-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function CardOne({ gameInfo, steamInfo, epicInfo }) {
+  const formattedTitle = gameInfo.scrapeTitle.replace(/ /g, "-");
 
-  const formattedTitle = gameInfo.scrapeTitle.replace(/ /g, '-');
+  function checkBorder(finalEpic, FinalSteam) {
+    let epicPrice = parseFloat(finalEpic.replace("$", ""));
+    let steamPrice = parseFloat(FinalSteam.replace("$", ""));
+
+    if (epicPrice < steamPrice) {
+      return "epic";
+    } else if (steamPrice < epicPrice) {
+      return "steam";
+    } else {
+      return "none";
+    }
+  }
 
   function truncateDescription(description) {
     const wordLimit = 10;
@@ -12,14 +25,13 @@ export default function CardOne({ gameInfo, steamInfo, epicInfo }) {
     if (words.length > wordLimit) {
       return words.slice(0, wordLimit).join(" ") + " ...";
     }
-
     return description;
   }
 
   return (
     <>
       <Card
-        className='mb-12 max-w-sm bg-black/50 backdrop-blur-md border border-gray-700/50 shadow-lg rounded-lg overflow-hidden bottom-0 z-10'
+        className='mb-12 max-w-sm bg-black/50 backdrop-blur-md border border-gray-700/50 shadow-lg rounded-lg overflow-hidden bottom-0 z-10 hover:scale-105 transition-transform duration-300 ease-in-out'
         renderImage={() => (
           <Image
             width={500}
@@ -35,14 +47,18 @@ export default function CardOne({ gameInfo, steamInfo, epicInfo }) {
         <p className='font-normal text-gray-400 dark:text-gray-400'>
           {truncateDescription(gameInfo.description)}
         </p>
-        <p className='font-bold text-white text-xs italic'>
-          Action , Racing , Multiplayer
+        <p className='font-semibold text-white text-sm capitalize'>
+          {gameInfo.genres.join(", ")}
         </p>
 
         <a
           href={`https://store.steampowered.com/search/?term=${gameInfo.scrapeTitle}`}
           target='_blank'
-          className='justify-between inline-flex w-full items-center rounded-lg bg-gray-800 px-4 py-2.5 text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 sm:w-auto'
+          className={`justify-between inline-flex w-full items-center rounded-lg bg-gray-800 px-5 py-2.5 text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-300 sm:w-auto ${
+            checkBorder(epicInfo.final_price, steamInfo.final_price) === "steam"
+              ? "border-lime-400 border"
+              : ""
+          }`}
         >
           <Image
             src='/steamLogo.png'
@@ -51,34 +67,65 @@ export default function CardOne({ gameInfo, steamInfo, epicInfo }) {
             width={50}
             height={50}
           />
+          {steamInfo.discount !== "N/A" && (
+            <div className='text-left'>
+              <div className='mb-1 text-xs text-lime-400'>
+                {steamInfo.discount}
+              </div>
+              <div className='-mt-1 font-sans text-md font-semibold line-through'>
+                {steamInfo.original_price}
+              </div>
+            </div>
+          )}
           <div className='text-left'>
             <div className='mb-1 text-xs'>Steam Store</div>
-            <div className='-mt-1 font-sans text-sm font-semibold'>$22.99</div>
+            <div className='-mt-1 font-sans text-md font-semibold'>
+              {steamInfo.final_price}
+            </div>
           </div>
         </a>
         <a
           href={`https://store.epicgames.com/en-US/p/${formattedTitle}`}
           target='_blank'
-          className='justify-between inline-flex w-full items-center rounded-lg border-lime-400 border bg-gray-800 px-4 py-2.5 text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-300 sm:w-auto'
+          className={`justify-between inline-flex w-full items-center rounded-lg bg-gray-800 px-5 py-2.5 text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-300 sm:w-auto 
+          ${
+            checkBorder(epicInfo.final_price, steamInfo.final_price) === "epic"
+              ? "border-lime-400 border"
+              : ""
+          }`}
         >
           <Image
             src='/epicLogo.png'
             className=''
-            alt='Steam_logo'
+            alt='Epic_logo'
             width={50}
             height={50}
           />
-          <div className='text-left'>
-            <div className='mb-1 text-xs text-lime-400'>-70%</div>
-            <div className='-mt-1 font-sans text-sm font-semibold line-through'>
-              $22.99
+          {epicInfo.discount !== "N/A" && (
+            <div className='text-left'>
+              <div className='mb-1 text-xs text-lime-400'>
+                {epicInfo.discount}
+              </div>
+              <div className='-mt-1 font-sans text-md font-semibold line-through'>
+                {epicInfo.original_price}
+              </div>
             </div>
-          </div>
+          )}
           <div className='text-left'>
             <div className='mb-1 text-xs'>Epic Store</div>
-            <div className='-mt-1 font-sans text-sm font-semibold'>$22.99</div>
+            <div className='-mt-1 font-sans text-md font-semibold'>
+              {epicInfo.final_price}
+            </div>
           </div>
         </a>
+        <div className='text-end pr-2'>
+          <Link
+            href={`/view-game/${gameInfo.scrapeTitle}`}
+            className='text-sm hover:text-blue-500'
+          >
+            See more ➤➤
+          </Link>
+        </div>
       </Card>
     </>
   );
