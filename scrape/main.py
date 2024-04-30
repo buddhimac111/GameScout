e1f461241426a1678b84ee86a720bcab76f20028
buddhimac111 <91274532+buddhimac111@users.noreply.github.com>
2024-04-30 16:56:15 +0530

    🎨: python scraping scrips updated

--- scrape/main.py
+++ scrape/main.py
@@ -4,16 +4,16 @@ from epic import get_epic_data
 from mongo import insert_document
 
 try:
-    # scrape data from the steam store
-    print("Scraping data from the Steam store, please wait..........")
-    steamData = get_steam_data(game_list)
-    print("Steam Scrape Completed")
-    print(steamData)
+    # # scrape data from the steam store
+    # print("Scraping data from the Steam store, please wait..........")
+    # steamData = get_steam_data(game_list)
+    # print("Steam Scrape Completed")
+    # print(steamData)
 
-    # save steam data to mongo
-    print("Inserting steam data into the database, please wait..........")
-    insert_document("steam", steamData)
-    print("Steam Data successfully inserted into the database")
+    # # save steam data to mongo
+    # print("Inserting steam data into the database, please wait..........")
+    # insert_document("steam", steamData)
+    # print("Steam Data successfully inserted into the database")
 
 
     # scrape data from the epic store
